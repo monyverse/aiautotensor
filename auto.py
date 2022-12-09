a49da725d89ab63f281318fd1180ade8959211db
Mac Thrasher <95183714+quac88@users.noreply.github.com>
2022-12-09 13:39:46 -0600

    fix deploy server and registration

--- auto.py
+++ auto.py
@@ -3,7 +3,8 @@ import subprocess
 import cubit
 import bittensor as bt
 
-# Define the is_registered() function
+
+# Check if registerred function
 def is_registered(wallet, subtensor: 'bt.Subtensor' = None) -> bool:
     """Returns true if this wallet is registered.
     Args:
@@ -19,14 +20,14 @@ def is_registered(wallet, subtensor: 'bt.Subtensor' = None) -> bool:
         subtensor = bt.subtensor()
     return subtensor.is_hotkey_registered(wallet.hotkey.ss58_address)
 
-# Define the deploy_core_server function
+# Get the current VRAM usuage of each GPU and define a threshold
 def deploy_core_server(vr_threshold: int = 4800, vram_used: list = []):
     """Deploys a bittensor core_server on a GPU with VRAM usage below the specified threshold.
        Args:
-               vr_threshold: VRAM threshold. If below threshold, deploy a Server
-               vram_used: List of VRAM usage values (bytes)
+               vr_threshold: VRAM threshold below which a bittensor core_server is deployed.
+               vram_used: List of VRAM usage values, in bytes.
        """
-    # Check nvidia-smi to get VRAM usage per GPU
+    # Use the nvidia-smi to get the VRAM usage of each GPU
     output = subprocess.check_output(["nvidia-smi", "--query-gpu=memory.used", "--format=csv,noheader,nounits"])
 
     # Parse the output to get the VRAM usage of each GPU
@@ -35,24 +36,27 @@ def deploy_core_server(vr_threshold: int = 4800, vram_used: list = []):
         if line == b'':
             continue
 
-        # Convert VRAM usage from bytes to MB
+        # Convert the VRAM usuage from bytes to MB
         vram_used = int(line.strip())
 
-        # If the VRAM usage < threshold, deploy core_server
+        # If the VRAM usage is below the threshold, deploy a Miner
         if vram_used < vr_threshold:
             print(f"Deploying bittensor core_server on GPU with {vram_used} MB of VRAM")
-            # Run core_server
-            config = bt.config()
-            subtensor = bt.subtensor(network = "nakamoto")
-            wallet = bt.wallet()
-            axon = bt.axon()
+            # Load the core_server model
+            config = None
+            # dataset = bt.dataset()
+            subtensor = bt.subtensor(network = "<>")
+            wallet = bt.wallet(name="<>", hotkey="<>")
+            axon = bt.axon(port = <>, wallet = wallet)
             metagraph = bt.metagraph()
-
-            # Run the core server
-            template = bt.neurons.core_server.neuron(config, subtensor, wallet, axon, metagraph).run()
+            
+            # RUN SERVERS OR VALIDATORS. UNCOMMENT
+            
+            # template = bt.neurons.core_server.neuron(config = config, subtensor = subtensor, wallet = wallet, axon = axon, metagraph = metagraph).run()
+            # template = bt.neurons.core_validator.neuron(config = config, subtensor = subtensor, wallet = wallet, axon = axon, metagraph = metagraph).run()
             pass
 
-# Create an array for GPUs
+# Create an array of the total number of GPUs
 def create_array():
     # Get the number of GPUs
     num_gpus = torch.cuda.device_count()
@@ -60,9 +64,10 @@ def create_array():
     # Create a list of numbers from 0 to num_gpus
     range_array = list(range(num_gpus))
 
-    # Concatenate the elements of the range_array list into a string with a space between each number
+    # Concatenate the elements of the range_array list into a single string with a space between each number
     range_string = ' '.join(str(x) for x in range_array)
 
+    # Return the string
     return range_string
 
 
@@ -72,20 +77,25 @@ num_gpus = torch.cuda.device_count()
 # Define a list of wallets
 wallets = []
 for i in range(num_gpus):
+    # Create a new wallet object for each GPU
     wallet = bt.wallet(name="<>", hotkey="<>")
     wallets.append(wallet)
-    
+
     # Check if the wallet is registered
     if not is_registered(wallet):
         # Create a subtensor object for the current GPU
         subtensor = bt.subtensor()
 
-        # Register the wallet
+        # Register the wallet using the current GPU
         range_string = create_array()
-        command = "btcli register --subtensor.network local --wallet.name {} --wallet.hotkey {} --cuda --cuda.dev_id {} --cuda.TPB 512 --cuda.update_interval 70_000 --no_prompt".format(wallet.name, range_string)
 
-        # Run on command line
+        # import pdb ### debugging
+        # pdb.set_trace() ### debugging
+        
+        #registration command 
+        command = "btcli register --subtensor.network local --wallet.name {} --wallet.hotkey {} --cuda --cuda.dev_id {} --cuda.TPB 512 --cuda.update_interval 250_000 --no_prompt".format(wallet.name, wallet.hotkey_str, range_string)
+
+        # Run the command in the command line
         subprocess.run(command, shell=True)
     else:
-        # Run a Server
         deploy_core_server()
