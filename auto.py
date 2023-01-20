ef9aa42872ae1d5efbb4e759b66c4067358f70c1
Mac Thrasher <95183714+quac88@users.noreply.github.com>
2023-01-20 15:39:30 -0600

    TPB

--- auto.py
+++ auto.py
@@ -1,23 +1,31 @@
 import argparse
-import os
 import subprocess
 from time import sleep
+from typing import Optional
+import math
+from typing import List, Optional, Sequence
+from collections import defaultdict
+import torch
+from bittensor import dataset as btdataset
+from datasets import load_dataset
+from tqdm import tqdm
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+from sklearn import metrics
+
 import bittensor as bt
 import torch
+
 import yaml
-from rich.prompt import Confirm, Prompt, PromptBase
 
+with open('config.yaml', 'r') as file:
+    machs = yaml.safe_load(file)
+    machine_config1 = machs['machine1']
+    machine_config2 = machs['machine2']
 
-machine_id = Prompt.ask(
-    "Which machine id is this?",
-)
-NOTIF = Prompt.ask(
-    "Would you like to turn on discord notifications?",
-    choices=["yes", "no"],
-    default="yes",
-)
-if NOTIF == "yes":
-    API_KEY = Prompt.ask("Enter your discord api key", default="")
+    #print(machs['machine1']['gpu0']['name'])
+
+#NETWORK = "nobunaga"
 
 
 # Define the is_registered() function
@@ -37,35 +45,20 @@ def is_registered(wallet, network, subtensor: "bt.Subtensor" = None) -> bool:
     return subtensor.is_hotkey_registered(wallet.hotkey.ss58_address)
 
 
-def check_is_running(proc_name: str) -> bool:
-    pm2_output = subprocess.check_output(["pm2", "id", proc_name])
-    return len(pm2_output) > 3
-
-
-def kill_pm2(proc_name: str):
-    os.system(f"pm2 delete {proc_name}")
-
-
-def make_proc_name(gpu_config, wallet) -> str:
-    return f"{gpu_config['name']}_{wallet.hotkey_str}"
-
-
 # Define the deploy_core_server function
-def deploy_core_server(gpu_index, gpu_config, wallet: "bt.Wallet"):
-
-    pm2_process_name = make_proc_name(gpu_config, wallet)
-    is_running = check_is_running(pm2_process_name)
+def deploy_core_server(
+    gpu_index,
+    gpu_config,
+    wallet: "bt.Wallet"
+):
+    is_running = False
     if is_running is False:
         command = (
-            f"pm2 start "
-            f"~/.bittensor/bittensor/bittensor/_neuron/text/core_server/main.py "
-            f"--name {pm2_process_name} --time --interpreter python3 -- "
+            f"--name autominer_{wallet.hotkey_str} --time --interpreter python3 -- "
             f"--logging.debug "
             f"--subtensor.network {gpu_config['network']} "
             f"--neuron.device cuda:{gpu_index} "
-            f"--neuron.model_name {gpu_config['model']} "
-            f"--axon.port {gpu_config['port']} "
-            f"--wallet.name {gpu_config['wallet']} "
+            f"--wallet.name test "
             f"--wallet.hotkey {wallet.hotkey_str}"
         )
         # Run the command in the command line
@@ -75,78 +68,53 @@ def deploy_core_server(gpu_index, gpu_config, wallet: "bt.Wallet"):
         is_running = True
         pass
 
+parser = argparse.ArgumentParser(
+    prog="Autominer", description="drains your wallet and immediately burns all your Tao"
+)
+parser.add_argument(
+    "--num_gpus",
+    default=0,
+    type=int,
+    help="How many GPUs to use for registration. Defaults to all available GPUs.",
+)
 
-num_gpus = torch.cuda.device_count()
+args = parser.parse_args()
 
-with open("config.yaml", "r") as file:
-    machs = yaml.safe_load(file)
+# Get the number of GPUs on the system
+if args.num_gpus == 0:
+    num_gpus = torch.cuda.device_count()
+else:
+    num_gpus = args.num_gpus
 
-if os.getenv("MACHINE_ID") is None:
-    os.environ["MACHINE_ID"] = machine_id
-assert os.getenv("MACHINE_ID") in machs.keys()
 
 
-while True:
-    for machine_id in machs.keys():
-        machine_config = machs[machine_id]
-        for gpu_index, gpu_config in enumerate(machine_config):
-            # Create a new wallet object for each GPU
-            wallet = bt.wallet(
-                name=(gpu_config["wallet"]),
-                path="auto_wallets/",
-                hotkey=str(gpu_config["keyfile"]),
-            )
-            # Check if the wallet is registered
-            if not is_registered(
-                wallet, network=gpu_config["network"]
-            ):  # --> THIS NEEDS TO BE AN ENVAR
-                expected_proc_name = make_proc_name(
-                    gpu_config=gpu_config, wallet=wallet
-                )
-                if check_is_running(expected_proc_name):
-                    kill_pm2(expected_proc_name)
-                    if NOTIF == "yes":
-                        command = f'curl -H "Content-Type: application/json" -d \'{{"content": "@here The {wallet.hotkey_str} key on {machine_id} has been deregistered!"}}\' "{API_KEY}"'
-                        print(command)
-                        subprocess.run(command, shell=True)
-                    sleep(10)
-                sleep(10)
+for gpu_index, gpu_config in enumerate(machine_config1):
+    # Create a new wallet object for each GPU
+    wallet = bt.wallet(name=(gpu_config['wallet']), hotkey=str(gpu_config['keyfile']))
+    #wallet = bt.wallet(name="test", hotkey=str(i))
+    wallet.create()
 
-            while not is_registered(wallet, network=gpu_config["network"]):
+    # Check if the wallet is registered
+    while not is_registered(wallet, network=gpu_config['network']):
 
-                range_string = " ".join(str(i) for i in range(num_gpus))
-                # Register the wallet using all GPUs. --> HOW TO SET TO HALF OR QUARTER TPB IF GPU IS ALREADY SERVING OR TURN REGISTRATION OFF. --> make this an ENVAR
-                if NOTIF == "yes" and machine_id == "machine1":
-                    command = (
-                        f"btcli register "
-                        f"--subtensor.network {gpu_config['network']} "
-                        f"--wallet.name {wallet.name} "
-                        f"--wallet.hotkey {wallet.hotkey_str} "
-                        f"--wallet.path auto_wallets/ "
-                        f"--cuda --cuda.dev_id {range_string} "
-                        f"--cuda.TPB 512 "
-                        f"--cuda update_interval 250_000 "
-                        f"--no_prompt "
-                    )
-                    command += f'&& curl -H "Content-Type: application/json" -d \'{{"content": "The {wallet.hotkey_str} key on {machine_id} has been registered!"}}\' "{API_KEY}"'
-                    print(command)
-                    subprocess.run(command, shell=True)
-                else:
-                    command = (
-                        f"btcli register "
-                        f"--subtensor.network {gpu_config['network']} "
-                        f"--wallet.name {wallet.name} "
-                        f"--wallet.hotkey {wallet.hotkey_str} "
-                        f"--wallet.path auto_wallets/ "
-                        f"--cuda --cuda.dev_id {range_string} "
-                        f"--cuda.TPB 512 "
-                        f"--cuda update_interval 250_000 "
-                        f"--no_prompt "
-                    )
-                    print(command)
-                    subprocess.run(command, shell=True)
+        range_string = " ".join(str(i) for i in range(num_gpus))  # not sorry ala
+        # Register the wallet using all GPUs.
+        command = (
+            f"btcli register "
+            f"--subtensor.network {gpu_config['network']} "
+            f"--wallet.name {wallet.name} "
+            f"--wallet.hotkey {wallet.hotkey_str} "
+            f"--cuda --cuda.dev_id {range_string} "
+            f"--cuda.TPB 512 "
+            f"--cuda update_interval 250_000 "
+            f"--no_prompt "
+        )
+        command += "&& curl -H \"Content-Type: application/json\" -d '{\"content\": \"@here a new key is Registered!\"}' \"https://canary.discord.com/api/webhooks/1040675301476278383/fxw42Qu0zduw-Ivk7_avfPK4_RuVoygO3nOQauEZHjZbHPZUtrf_AFQTOIMJxW9JVjqf\""
+        print(command)
+        subprocess.run(command, shell=True)
 
-            if machine_id == os.getenv("MACHINE_ID"):
-                deploy_core_server(gpu_index, gpu_config, wallet)
+    else:
 
-            sleep(10)
+
+    deploy_core_server(gpu_index, gpu_config, wallet)
+    sleep(10)
